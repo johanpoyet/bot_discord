735fe3385052d62c915ec9845ab9c1df1cb69979
johanpoyet <johan.poyet@clsi.fr>
2023-01-16 14:30:29 +0100

    test

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token:"MTA2MDE0ODkzMzcxNzkzODE3Ng.GRu3BZ._TtRhoZfnBxFqoP6yUMTKHI0WYcn_ZbbUosTCM",
+    api_key:"1df1df00-4161-461d-90ac-5c0f9903927e",
+    api_url:"https://api.fortnitetracker.com/v1/"
+}
